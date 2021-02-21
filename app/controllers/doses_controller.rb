917d52ed1136d86edcf287dcd0eda5dfc56a5943
arturo quiroz <r2inlondon@gmail.com>
2021-02-21 12:59:08 -0600

    Doses controller and view completed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -16,15 +16,14 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
-  end
-
-  def find_cocktail
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: %i[index show new create] do
-    resources :doses, only: %i[create new destroy]
+    resources :doses, only: %i[create new]
   end
   root to: 'cocktails#index'
+  resources :doses, only: [:destroy]
 end
