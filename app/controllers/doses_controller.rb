4cfc255585a1e419a07d5bad2224f2bcfb423f28
arturo quiroz <r2inlondon@gmail.com>
2021-02-21 10:21:54 -0600

    doses new and  create completed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,30 @@
 class DosesController < ApplicationController
+  # def new
+  #   @dose = Dose.new
+  # end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save!
+      redirect_to cocktail_path(@cocktail)
+    else
+      @dose = Dose.new
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def find_cocktail
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails, only: %i[index show new create] do
-    root to: 'cocktails#index'
     resources :doses, only: %i[create new destroy]
   end
+  root to: 'cocktails#index'
 end
