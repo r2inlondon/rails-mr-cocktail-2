a4d23645aa65ea0a13e45fbbfc18f7403711a819
arturo quiroz <r2inlondon@gmail.com>
2021-02-20 22:04:14 -0600

    Doses routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: %i[index show new create]
-  root to: 'cocktails#index'
+  resources :cocktails, only: %i[index show new create] do
+    root to: 'cocktails#index'
+    resources :doses, only: %i[create new destroy]
+  end
 end
